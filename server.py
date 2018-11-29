d4178fcb1a85f9c06ed638ee814ad66e639af1e3
mick <mccoyc@cat.pdx.edu>
2018-11-29 12:52:37 -0800

    allow multiple client connections

--- server.py
+++ server.py
@@ -1,24 +1,34 @@
 import socket
 import sys
+import select
 
 def main():
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
   host = ''
   port = int(sys.argv[1])
-  s.bind((host, port))
+  server.bind((host, port))
 
-  s.listen(5)
+  server.listen(5)
+  input = [server]
+  
   while(1):
-    conn, addr = s.accept()
-    print('client is at', addr)
+    inputready,outputready,exceptready = select.select(input,[],[])
 
-    while(1): 
-      data = conn.recv(1024)
-      #data = data * 1000
-      if not data:
-        break
-      conn.sendall(data)
+    for s in inputready:
+
+      if s == server:
+        conn, addr = s.accept()
+        print('client is at', addr)
+        input.append(conn)
+
+      else:
+        #while(1): 
+        data = s.recv(1024)
+          #data = data * 1000
+        if not data:
+          break
+        s.sendall(data)
   #conn.close()
 
 if __name__ == '__main__':
